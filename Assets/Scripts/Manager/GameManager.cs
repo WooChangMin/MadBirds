8da7b0670d7e5406e23acd2b670c3ea4f24f0dba
우창민 <rebuilding33@gmail.com>
2023-09-26 14:02:36 +0900

    feat : Add scoreManager

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
     private static SoundManager soundManager;
     private static SceneManager sceneManager;
     private static UIManager uiManager;
+    private static ScoreManager scoreManager;
 
     public static GameManager Instance { get { return instance; } }
     public static SoundManager Sound { get { return soundManager; } }
     public static SceneManager Scene { get { return sceneManager; } }
     public static UIManager UI { get { return uiManager; } }
+    public static ScoreManager Score { get { return scoreManager; } }
 
     private void Awake()
     {
@@ -40,5 +42,10 @@ public class GameManager : MonoBehaviour
         uiObj.name = "UIManager";
         uiObj.transform.parent = transform;
         uiManager = uiObj.AddComponent<UIManager>();
+
+        GameObject scoreObj = new GameObject();
+        scoreObj.name = "UIManager";
+        scoreObj.transform.parent = transform;
+        scoreManager = scoreObj.AddComponent<ScoreManager>();
     }
 }
