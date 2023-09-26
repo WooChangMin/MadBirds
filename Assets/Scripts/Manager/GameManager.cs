9ea6fa6e84ee130b991581c097367e2b5a9792a5
우창민 <rebuilding33@gmail.com>
2023-09-26 13:21:43 +0900

    초기 커밋.

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    private static SoundManager soundManager;
+    private static SceneManager sceneManager;
+    private static UIManager uiManager;
+
+    public static GameManager Instance { get { return instance; } }
+    public static SoundManager Sound { get { return soundManager; } }
+    public static SceneManager Scene { get { return sceneManager; } }
+    public static UIManager UI { get { return uiManager; } }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+        InitManagers();
+    }
+
+    private void InitManagers()
+    {
+        GameObject sceneObj = new GameObject();
+        sceneObj.name = "SceneManager";
+        sceneObj.transform.parent = transform;
+        sceneManager = sceneObj.AddComponent<SceneManager>();
+
+        GameObject soundObj = new GameObject();
+        soundObj.name = "SoundManager";
+        soundObj.transform.parent = transform;
+        soundManager = soundObj.AddComponent<SoundManager>();
+
+        GameObject uiObj = new GameObject();
+        uiObj.name = "UIManager";
+        uiObj.transform.parent = transform;
+        uiManager = uiObj.AddComponent<UIManager>();
+    }
+}
